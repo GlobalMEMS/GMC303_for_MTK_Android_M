5cdb79d964e892f6486345434277fea03db19eeb
SteveHuang27 <SteveHuang27@users.noreply.github.com>
2016-12-17 10:31:09 +0800

    GMC303 drivers and daemon for MTK 6797 Android M

---
+++ alps/vendor/amt/libs/mt6797_64/gmadfs303/softiron.h
@@ -0,0 +1,9 @@
+#ifndef __softiron_h
+#define __softiron_h
+
+#define SOFT_IRON_MATRIX {0.676401,0.008204,-0.00206,0.006437,1.349951,0.008504,-0.151822,-0.000484,1.094876}
+
+//#define SOFT_IRON_MATRIX {1,0,0,0,1,0,0,0,1}
+#define SOFT_IRON_Version 1.0
+
+#endif
